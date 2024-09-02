9188aa9d72001ae937db4420ffc9f892701b1473
Tman-noob <91443186+Tman-noob@users.noreply.github.com>
2024-09-02 10:28:01 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 let countEl = document.getElementById("count-el")
-let count = 0;
 let saveEl = document.getElementById("save-el");
+let count = 0;
+
 
 function increment() {
     count += 1;
-    countEl.innerText = count;
+    countEl.textContent = count;
+}
+
+function decrease() {
+    count -= 1;
+    countEl.textContent = count;
 }
 
 function save() {
-    saveEl.innerText += " " + count + " - ";
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
 }
-
-
-
-// let username = "Tim"
-// let message = "You have tree new notifications"
-// let messageToUser = message + ", " + username + "!";
-// console.log(messageToUser)
-
-// let name = "Tim"
-// let greeting = "Hi, my name is ";
-// let myGreeting = greeting + name;
-// console.log(myGreeting)
